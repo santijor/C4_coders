0fda9c1194ef4394f3345f614d1ad9bc71413c90
santijor <153324381+santijor@users.noreply.github.com>
2025-05-30 17:52:35 -0300

    cree un primer nodo (nodo1) y el metodo para armar otro nodo (nodo2)

--- lista_enlazada_c4coders.c
+++ lista_enlazada_c4coders.c
@@ -3,8 +3,22 @@
 
 //Las explicaciones se harán por separado en cada sección del código.
 
-// Estructura del nodo
-typedef struct Nodo {
+/**Estructura del nodo. La variable dato de tipo int almacena el valor del nodo.
+Se crea un puntero hacia el siguiente nodo.
+**/
+
+typedef struct Nodo1 {
     int dato;
-    struct Nodo* siguiente;
-} Nodo;
+    struct Nodo1* siguiente;
+} Nodo1;
+
+Nodo1* crearNodo(int dato) {
+    Nodo1* nuevoNodo2 = (Nodo1*)malloc(sizeof(Nodo1));
+    if (nuevoNodo2 == NULL) {
+        printf("Error al asignar memoria.\n");
+        exit(1);
+    }
+    nuevoNodo2->dato = dato;
+    nuevoNodo2->siguiente = NULL;
+    return nuevoNodo2;
+}
